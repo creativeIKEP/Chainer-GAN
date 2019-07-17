7e09f6a14ccac44a0ec8d5e044c368bed67423d1
IKEP <syunya.i.1021@gmail.com>
2019-07-17 14:54:07 +0900

    apply gpu

--- dcgan/train.py
+++ dcgan/train.py
@@ -5,21 +5,24 @@ import chainer
 from chainer import Variable
 from chainer.iterators import SerialIterator
 import chainer.functions as F
+import cupy as xp
+from PIL import Image
 
 
 def train():
     dataset_folder_path = "dataset"
     n_hidden = 300
     epoch_count = 50
-    batch_size = 1
+    batch_size = 64
 
     dataset = data_io.dataset_load(dataset_folder_path)
     train_iter = SerialIterator(dataset, batch_size, repeat=True, shuffle=True)
 
     gen = Generator(n_hidden=n_hidden)
     dis = Discriminator()
-    #gen.to_gpu(0)
-    #dis.to_gpu(0)
+
+    gen.to_gpu(0)
+    dis.to_gpu(0)
 
     opt_gen = chainer.optimizers.Adam()
     opt_gen.setup(gen)
@@ -33,10 +36,10 @@ def train():
         while True:
             mini_batch_images = train_iter.next()
 
-            x_real = Variable(np.array(mini_batch_images))
+            x_real = Variable(xp.array(mini_batch_images))
             y_real = dis(x_real)
 
-            noise = np.random.uniform(-1, 1, (batch_size, n_hidden)).astype(np.float32)
+            noise = xp.random.uniform(-1, 1, (batch_size, n_hidden)).astype(xp.float32)
             z = Variable(noise)
             x_fake = gen(z, (batch_size))
             y_fake = dis(x_fake)
@@ -50,6 +53,9 @@ def train():
             opt_dis.update()
             opt_gen.update()
 
+            d_loss.to_cpu()
+            g_loss.to_cpu()
+
             iteration += batch_size
             d_loss_list.append(d_loss.array)
             g_loss_list.append(g_loss.array)
@@ -57,20 +63,27 @@ def train():
             if train_iter.is_new_epoch:
                 break
 
+        x_fake.to_cpu()
+        generated_images = x_fake.array
+        n, ch, h, w = generated_images.shape
+        generated_images = generated_images.reshape(n, h, w, 3)
+        print(generated_images.shape)
+        Image.fromarray(generated_images[0].astype(np.uint8)).save(
+                    "output/" + str(epoch)+".png")
         print("epoch: " + str(epoch+1) + ", interation: " + str(iteration) + ", d_loss: " + str(np.mean(d_loss_list)) + ", g_loss: " + str(np.mean(g_loss_list)))
 
 
 
 def loss_dis(batchsize, y_real, y_fake):
-    real_loss = F.softmax_cross_entropy(y_real, Variable(np.ones(batchsize, dtype=np.int32)))
-    fake_loss = F.softmax_cross_entropy(y_fake, Variable(np.zeros(batchsize, dtype=np.int32)))
+    real_loss = F.softmax_cross_entropy(y_real, Variable(xp.ones(batchsize, dtype=xp.int32)))
+    fake_loss = F.softmax_cross_entropy(y_fake, Variable(xp.zeros(batchsize, dtype=xp.int32)))
     L1 = F.sum(real_loss) / batchsize
     L2 = F.sum(fake_loss) / batchsize
     return L1 + L2
 
 
 def loss_gen(batchsize, y_fake):
-    fake_loss = F.softmax_cross_entropy(y_fake, Variable(np.ones(batchsize, dtype=np.int32)))
+    fake_loss = F.softmax_cross_entropy(y_fake, Variable(xp.ones(batchsize, dtype=xp.int32)))
     loss = F.sum(fake_loss) / batchsize
     return loss
 
