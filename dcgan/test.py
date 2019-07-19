9826d5ea8a55ff344ec694e73db0a20678c9925e
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2019-07-19 10:39:43 +0900

    fix test file

--- dcgan/test.py
+++ dcgan/test.py
@@ -1,11 +1,13 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import chainer
 from net import Generator
 
 
-def test(n_hidden):
+def test(n_hidden, npz_path):
     gen = Generator(n_hidden=n_hidden)
+    chainer.serializers.load_npz(npz_path, gen)
     noise = np.random.uniform(-1, 1, (1, n_hidden), dtype=np.float32)
     z = Variable(noise)
     x_fake = gen(z, 1)
@@ -20,6 +22,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Chainer-GAN: dcgan')
     parser.add_argument('-n_hidden', '-z', type=int, default=100,
                         help='Size of noise to input Generator. Please input same size when dcgan train')
+    parser.add_argument('-path', '-p'
+                        help='Trained Generator path. Please input trained generator npz file path')
     args = parser.parse_args()
-    
-    test(args.n_hidden)
+
+    test(args.n_hidden, args.path)
