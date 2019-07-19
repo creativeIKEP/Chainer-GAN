bd5e3d92c3b7cfa59d3ae7253796c38aca262ffc
IKEP <syunya.i.1021@gmail.com>
2019-07-19 10:56:26 +0900

    fix train setting in test file

--- dcgan/test.py
+++ dcgan/test.py
@@ -2,20 +2,24 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import chainer
+from chainer import Variable
 from net import Generator
 
 
 def test(n_hidden, npz_path):
-    gen = Generator(n_hidden=n_hidden)
-    chainer.serializers.load_npz(npz_path, gen)
-    noise = np.random.uniform(-1, 1, (1, n_hidden), dtype=np.float32)
-    z = Variable(noise)
-    x_fake = gen(z, 1)
-    print(x_fake)
+    with chainer.using_config("train", False):
+        gen = Generator(n_hidden=n_hidden)
+        chainer.serializers.load_npz(npz_path, gen)
+        noise = np.random.uniform(-1, 1, (1, n_hidden)).astype(np.float32)
+        z = Variable(noise)
+        x_fake = gen(z, 1)
+        generated_image = x_fake.array
+        generated_image = generated_image.transpose(0, 2, 3, 1)
+        generated_image = np.clip(generated_image[0] * 255, 0.0, 255.0).astype(np.uint8)
 
-    im_list = np.asarray(x_fake)
-    plt.imshow(im_list)
-    plt.show()
+        im_list = np.asarray(generated_image)
+        plt.imshow(im_list)
+        plt.show()
 
 
 if __name__ == '__main__':
